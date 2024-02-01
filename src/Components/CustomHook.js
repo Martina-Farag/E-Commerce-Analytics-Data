ec22eb2a5310a369a4dabf398dcd70607ddb2e02
Martina-Farag <99760577+Martina-Farag@users.noreply.github.com>
2024-02-01 23:44:33 +0200

    Update CustomHook.js

--- src/Components/CustomHook.js
+++ src/Components/CustomHook.js
@@ -9,8 +9,10 @@ const CustomHook = () => {
 
     useEffect(() => {
 
+      const abortCont = new AbortController();
+
         // get products data from Fake Store API by axios library.
-        axiosInstance.get(`/products`,{
+        axiosInstance.get(`/products`, { signal: abortCont.signal
         }).then( result => {
             // if( !result.ok ){     // response.ok = true when response is optained successfully and returns false otherwise.
             //     throw new Error("Sorry, could not Get the data for that Resource"); // we here making an error to be cateched with .catch() method and make my error variable = true 
@@ -19,29 +21,37 @@ const CustomHook = () => {
             setProducts(result.data);
 
         }).catch( error => {
-            console.log(error.message);
+          if(error.name === 'AbortError'){
+            console.log('Axios aborted');
+          }
+          else {console.log(error.message);}
         })
 
         // get users data from Fake Store API by axios library.
-        axiosInstance.get(`/users`,{
+        axiosInstance.get(`/users`, { signal: abortCont.signal
         }).then( result => {
             setUsers(result.data);
 
         }).catch( error => {
-            console.log(error.message);
+          if(error.name === 'AbortError'){
+            console.log('Axios aborted');
+          }
+          else {console.log(error.message);}
         })
 
         // get carts data from Fake Store API by axios library.
-        axiosInstance.get(`/carts`,{
+        axiosInstance.get(`/carts`, { signal: abortCont.signal
         }).then( result => {
             setCarts(result.data);
               
         }).catch( error => {
-            console.log(error.message);
+          if(error.name === 'AbortError'){
+            console.log('Axios aborted');
+          }
+          else {console.log(error.message);}
         })
 
-
-
+        return () => abortCont.abort();
 
     }, []);     // only fires on the first load of the component
 
@@ -57,7 +67,6 @@ const CustomHook = () => {
     // salesGrowthMonths is (Frequency Array) : array of zeros to use it as counter to calc the sales grows every Month
     const [salesGrowthMonths, setSalesGrowthMonths] = useState();
     const [salesGrowthDays, setSalesGrowthDays] = useState();
-    const [dates, setDates] = useState([]);
 
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
@@ -69,12 +78,8 @@ const CustomHook = () => {
         var salesGrowthMonths = {};
         var salesGrowthDays = {};
 
-        var dates = [];
       
         if (products && users && carts) {
-
-// date.getMonth()
-          // console.log(salesGrowthMonths);
 
           // Calculate totalProductsPrice
           for (let i = 0; i < products.length; i++) {
@@ -84,7 +89,6 @@ const CustomHook = () => {
           // Calculate totalSalesNumber and totalIncome
           for (let i = 0; i < carts.length; i++) {
             const productsArray = carts[i].products;
-            dates[i] = carts[i].date;
       
             for (let j = 0; j < productsArray.length; j++) {
               totalSalesNumber += productsArray[j].quantity;
@@ -92,24 +96,17 @@ const CustomHook = () => {
 
               let cartDate = new Date(carts[i].date);
               if(salesGrowthMonths[month[cartDate.getMonth()]] ){
-                
                 salesGrowthMonths[month[cartDate.getMonth()]] += productsArray[j].quantity;
               }else {
                 salesGrowthMonths[month[cartDate.getMonth()]] = productsArray[j].quantity;
-                // console.log(month[cartDate.getMonth()]);
               }
 
               if(salesGrowthDays[days[cartDate.getMonth()]] ){
-                
                 salesGrowthDays[days[cartDate.getMonth()]] += productsArray[j].quantity;
               }else {
                 salesGrowthDays[days[cartDate.getMonth()]] = productsArray[j].quantity;
-                // console.log(days[cartDate.getMonth()]);
-
               }
-
               
-
             }
           }
       
@@ -122,10 +119,8 @@ const CustomHook = () => {
           setSalesGrowthMonths(salesGrowthMonths);
           setSalesGrowthDays(salesGrowthDays);
 
-          setDates(dates);
-
-          // console.log(salesGrowthMonths , dates);
         }
+
       }, [products, users, carts]);         // will run on any apdate of these variables
       
       // Helper function to get product price by id
@@ -135,7 +130,7 @@ const CustomHook = () => {
       }
 
       
-  return { products, users, carts, productsNumber, totalProductsPrice, usersNumber, cartsNumber, totalSalesNumber, totalIncome, salesGrowthMonths, salesGrowthDays, dates };
+  return { products, users, carts, productsNumber, totalProductsPrice, usersNumber, cartsNumber, totalSalesNumber, totalIncome, salesGrowthMonths, salesGrowthDays };
 }
  
 export default CustomHook;
